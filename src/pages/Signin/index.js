ee345f8028012ad320719a6c01c8768d9a38c18d
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-14 09:42:15 -0300

    import context and create function Signin

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,18 +1,30 @@
-import { useState } from 'react';
+import { useState, useContext } from 'react';
 import './signin.css';
 import logo from '../../assets/logo.png'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
 
 function SignIn(){
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const { signIn } = useContext(AuthContext);
+
+    function handleSignIn(e){
+        e.preventDefault();
+
+        if(email !== '' && password !== ''){
+            signIn(email, password);
+        }
+    }
+
     return(
         <div className='container-center'>
             <div className='login'>
                 <div className='login-area'>
                     <img src={logo} alt="Logo" />
                 </div>
-                <form>
+                <form onSubmit={handleSignIn}>
                     <h1>Entrar</h1>
                     <input 
                     type="text" 
