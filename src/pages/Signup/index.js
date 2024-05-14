b6fdbdadce0c0a2e044630f2c1d8b7a707035b04
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-14 08:19:26 -0300

    implementando um form na pagina de cadastro

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,7 +1,42 @@
+import { useState } from 'react'
+import logo from '../../assets/logo.png'
+import { Link } from 'react-router-dom'
+
 function SignUp(){
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
     return(
-        <div>
-            <h1>Pagina Cadastro</h1>
+        <div className='container-center'>
+            <div className='login'>
+                <div className='login-area'>
+                    <img src={logo} alt="Logo" />
+                </div>
+                <form>
+                    <h1>Nova Conta</h1>
+                    <input 
+                    type="text" 
+                    placeholder="Seu nome" 
+                    value={name}
+                    onChange={(e)=> setName(e.target.value)}
+                    />
+                    <input 
+                    type="text" 
+                    placeholder="email@eamil.com" 
+                    value={email}
+                    onChange={(e)=> setEmail(e.target.value)}
+                    />
+                    <input 
+                    type="password" 
+                    placeholder="*******" 
+                    value={password}
+                    onChange={(e)=> setPassword(e.target.value)}
+                    />
+
+                    <button type='submit'>Cadastrar</button>
+                </form>
+                <Link to="/">Ja possui uma conta? faça Login</Link>
+            </div>
         </div>
     )
 }
