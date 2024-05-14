52df976f837d8b50fafc79db41c7e4e3098a3dfa
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-14 11:29:37 -0300

    Update index.js

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,6 +1,6 @@
-import { useState } from 'react'
-import logo from '../../assets/logo.png'
-import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
 
 function SignUp(){
     const [name, setName] = useState('')
