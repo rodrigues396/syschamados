69de3637c6298e71678b288824ac5b8249307b8f
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-16 10:48:58 -0300

    create function logout with context

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,7 +1,17 @@
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
+
 function Dashboard(){
+    const { logout } = useContext(AuthContext)
+
+    async function handleLogout(){
+        await logout();
+    }
+
     return(
         <div>
             <h1>Pagina Dashboard</h1>
+            <button onClick={handleLogout}>Sair da conta</button>
         </div>
     )
 }
