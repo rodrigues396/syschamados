361b838370ab8ff60252c1ba399328b4cee040a8
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-20 14:03:35 -0300

    rendering sidebar

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,6 +1,8 @@
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 
+import Header from "../../components/Header";
+
 function Dashboard(){
     const { logout } = useContext(AuthContext)
 
@@ -10,6 +12,7 @@ function Dashboard(){
 
     return(
         <div>
+            <Header/>
             <h1>Pagina Dashboard</h1>
             <button onClick={handleLogout}>Sair da conta</button>
         </div>
