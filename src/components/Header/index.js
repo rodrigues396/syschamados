7da0c4ebeea1bbc2bab6a59f8f1d1c6195cb0fc2
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-20 14:02:54 -0300

    created sidebar

---
+++ src/components/Header/index.js
@@ -0,0 +1,35 @@
+import avatarImg from '../../assets/avatar.png'
+import { Link } from 'react-router-dom';
+import { AuthContext } from '../../contexts/auth';
+import { useContext } from 'react';
+
+import { FiHome, FiUser, FiSettings} from 'react-icons/fi';
+import './header.css';
+
+function Header(){
+    const { user } = useContext(AuthContext);
+
+    return(
+        <div className="sidebar">
+            <div>
+                <img src={user.avatarUrl === null ? avatarImg : user.avatarUrl} alt="Foto do Usuario" />
+            </div>
+            <Link to='/dashboard'>
+                <FiHome color="#FFF" size={24}/>
+                Chamados
+            </Link>
+
+            <Link to='/customers'>
+                <FiUser color="#FFF" size={24}/>
+                Clientes
+            </Link>
+
+            <Link to='/profile'>
+                <FiSettings color="#FFF" size={24}/>
+                Perfil
+            </Link>
+        </div>
+    )
+}
+
+export default Header;
