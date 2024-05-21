0650c1bd727085863a6027144928190eb6c7cb37
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-21 09:23:37 -0300

    showing profile data

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,9 +1,17 @@
 import Header from "../../components/Header";
 import Title from "../../components/Title"
 
-import { FiSettings } from "react-icons/fi";
+import { FiSettings, FiUpload } from "react-icons/fi";
+import avatar from '../../assets/avatar.png';
+import { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/auth";
+
+import './profile.css';
 
 function Profile(){
+    const {user} = useContext(AuthContext)
+    const [avatarUrl, setAvatarUrl] = useState(user && user.avatarUrl)
+
     return(
         <div>
             <Header />
@@ -12,9 +20,34 @@ function Profile(){
                 <Title name="Minha conta">
                     <FiSettings size={25}/>
                 </Title>
-            </div>
 
-            <h1>Perfil</h1>
+                <div className="container">
+                    <form className="form-profile">
+                    <label className="label-avatar">
+                        <span>
+                            <FiUpload color="#FFF" size={25} />
+                        </span>
+                        <input type="file" accept="image/*"/> <br/>
+                        {avatarUrl === null ? (
+                            <img src={avatar} alt="Foto de Perfil" width={250} height={250}/>
+                        ) : (
+                            <img src={avatarUrl} alt="Foto de Perfil" width={250} height={250}/>
+                        )}
+                    </label>
+
+                    <label>Nome</label>
+                    <input type="text" placeholder="Seu nome"/>
+                    <label>Email</label>
+                    <input type="text" placeholder="teste@gmail.com" disabled={true}/>
+
+                    <button type="submit">Salvar</button>
+
+                    </form>
+                </div>
+                <div className="container">
+                    <button className="logout-btn">Sair</button>
+                </div>
+            </div>
         </div>
     )
 }
