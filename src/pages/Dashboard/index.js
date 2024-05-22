29b3383ebfed2db7827f400305e14e95129ae255
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-22 14:20:00 -0300

    Fiat: create table static request

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -2,6 +2,13 @@ import { useContext } from "react";
 import { AuthContext } from "../../contexts/auth";
 
 import Header from "../../components/Header";
+import Title from "../../components/Title";
+
+import { FiPlus, FiMessageSquare, FiSearch, FiEdit2} from "react-icons/fi";
+
+import { Link } from "react-router-dom";
+
+import './dashboard.css';
 
 function Dashboard(){
     const { logout } = useContext(AuthContext)
@@ -13,8 +20,47 @@ function Dashboard(){
     return(
         <div>
             <Header/>
-            <h1>Pagina Dashboard</h1>
-            <button onClick={handleLogout}>Sair da conta</button>
+
+            <div className="content">
+                <Title name="Chamados">
+                    <FiMessageSquare size={25} />
+                </Title>
+
+                <>
+                <Link to='/new' className="new">
+                    <FiPlus color="#FFF" size={25}/>
+                    Novo Chamado
+                </Link>
+
+                    <table>
+                        <thead>
+                            <tr>
+                                <th scope="col">Cliente</th>
+                                <th scope="col">Assunto</th>
+                                <th scope="col">Status</th>
+                                <th scope="col">Cadastrando em</th>
+                                <th scope="col">#</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td data-label='Cliente'>Mercado Esquina</td>
+                                <td data-label='Assunto'>Suporte</td>
+                                <td data-label='Status'>Em Aberto</td>
+                                <td data-label='Cadastrado'>12/05/2024</td>
+                                <td data-label='#'>
+                                    <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                        <FiSearch color="#FFF" size={17} />
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#f5a935'}}>
+                                        <FiEdit2 color="#FFF" size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </>
+            </div>
         </div>
     )
 }
