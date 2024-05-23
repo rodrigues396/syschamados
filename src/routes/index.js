2e7efc40f0618bd184bc15abfc1b1ded40498cea
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-23 12:20:28 -0300

    Fiat: configuration route new
    novo chamados

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import SignIn from "../pages/Signin";
 import SignUp from "../pages/Signup";
 import Dashboard from "../pages/Dashboard";
+import New from "../pages/New";
 import Customers from "../pages/Customers";
 import Profile from "../pages/Profile";
 
@@ -13,7 +14,9 @@ function RoutesApp(){
         <Routes>
             <Route path="/" element={<SignIn/>} />
             <Route path="/register" element={<SignUp/>} />
+            
             <Route path="/dashboard" element={<Private><Dashboard/></Private>} />
+            <Route path="/new" element={<Private><New/></Private>} />
             <Route path="/customers" element={<Private><Customers/></Private>} />
             <Route path="/profile" element={<Private><Profile/></Private>} />
         </Routes>
