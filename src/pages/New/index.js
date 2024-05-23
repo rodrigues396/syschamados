96a01611959c962bac2d84a42ff1c8bae40a0ab5
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-23 12:33:24 -0300

    Fiat: created form in new

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -11,6 +11,55 @@ function New(){
                 <Title name="Novo Chamado">
                     <FiPlusCircle size={25}/>
                 </Title>
+
+                <div className="container">
+                    <form className="form-profile">
+                        <label>Clientes</label>
+                        <select>
+                            <option key={0} value="0">Mercado</option>
+                            <option key={2} value="1">Loja Informatica</option>
+                        </select>
+
+                        <label>Assunto</label>
+                        <select>
+                            <option value="Suporte">Suporte</option>
+                            <option value="Visita tecnica">Visita tecnica</option>
+                            <option value="Financeiro">Financeiro</option>
+                        </select>
+
+                        <label>Status</label>
+                        <div className="status">
+                            <input 
+                            type="radio"
+                            name="radio"
+                            value="Aberto"
+                            />
+                            <span>Em Aberto</span>
+
+                            <input 
+                            type="radio"
+                            name="radio"
+                            value="Progresso"
+                            />
+                            <span>Progresso</span>
+
+                            <input 
+                            type="radio"
+                            name="radio"
+                            value="Atendido"
+                            />
+                            <span>Atendido</span>
+                        </div>
+
+                        <label>Complemento</label>
+                        <textarea
+                        type="text"
+                        placeholder="Descreva seu problema (opcional)."
+                        />
+
+                        <button type="submit">Registrar</button>
+                    </form>
+                </div>
             </div>
         </div>
     )
