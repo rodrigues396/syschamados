761683f0190223b3346187693879409afed3f344
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-23 12:10:23 -0300

    new line in table

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -46,7 +46,11 @@ function Dashboard(){
                             <tr>
                                 <td data-label='Cliente'>Mercado Esquina</td>
                                 <td data-label='Assunto'>Suporte</td>
-                                <td data-label='Status'>Em Aberto</td>
+                                <td data-label='Status'>
+                                    <span className='badge' style={{backgroundColor: '#999'}}>
+                                    Em Aberto
+                                    </span>
+                                </td>
                                 <td data-label='Cadastrado'>12/05/2024</td>
                                 <td data-label='#'>
                                     <button className="action" style={{backgroundColor: '#3583f6'}}>
@@ -57,6 +61,28 @@ function Dashboard(){
                                     </button>
                                 </td>
                             </tr>
+
+
+
+                            <tr>
+                                <td data-label='Cliente'>Informatica TECH</td>
+                                <td data-label='Assunto'>Suporte</td>
+                                <td data-label='Status'>
+                                    <span className='badge' style={{backgroundColor: '#999'}}>
+                                    Em Aberto
+                                    </span>
+                                </td>
+                                <td data-label='Cadastrado'>12/05/2024</td>
+                                <td data-label='#'>
+                                    <button className="action" style={{backgroundColor: '#3583f6'}}>
+                                        <FiSearch color="#FFF" size={17} />
+                                    </button>
+                                    <button className="action" style={{backgroundColor: '#f5a935'}}>
+                                        <FiEdit2 color="#FFF" size={17} />
+                                    </button>
+                                </td>
+                            </tr>
+
                         </tbody>
                     </table>
                 </>
