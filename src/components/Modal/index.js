4ade22126b61c0feb9b99f9c5c917abe2aa8524a
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-28 11:40:11 -0300

    Fiat: content modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,9 +1,44 @@
 import './modal.css';
+import { FiX } from 'react-icons/fi';
 
 function Modal(){
     return(
-        <div>
+        <div className='modal'>
+            <div className='container'>
+                <button className='close'>
+                    <FiX size={25} color='#fff' />
+                    Voltar
+                </button>
+                <main>
+                    <h2>Detalhes do Chamado</h2>
 
+                    <div className='row'>
+                        <span>
+                            Cliente: <i>Mercado</i>
+                        </span>
+                    </div>
+
+                    <div className='row'>
+                        <span>
+                            Assunto: <i>Suporte</i>
+                        </span>
+                        <span>
+                            Cadastrado em: <i>27/05/2024</i>
+                        </span>
+                    </div>
+                    <div className='row'>
+                        <span>
+                            Status: <i>Aberto</i>
+                        </span>
+                    </div>
+                    <>
+                    <h3>complemento</h3>
+                    <p>
+                        Aqui vai todo o complemento do chamado
+                    </p>
+                    </>
+                </main>
+            </div>
         </div>
     )
 }
