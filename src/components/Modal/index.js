b838830efaf40f1195e5c23ba07cee016a48d3c4
Rodrigues396 <thallisonsiqueira@gmail.com>
2024-05-29 09:50:35 -0300

    Fiat: add content in modal

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,11 +1,11 @@
 import './modal.css';
 import { FiX } from 'react-icons/fi';
 
-function Modal(){
+function Modal({ conteudo, close}){
     return(
         <div className='modal'>
             <div className='container'>
-                <button className='close'>
+                <button className='close' onClick={close}>
                     <FiX size={25} color='#fff' />
                     Voltar
                 </button>
@@ -14,29 +14,38 @@ function Modal(){
 
                     <div className='row'>
                         <span>
-                            Cliente: <i>Mercado</i>
+                            Cliente: <i>{conteudo.cliente}</i>
                         </span>
                     </div>
 
                     <div className='row'>
                         <span>
-                            Assunto: <i>Suporte</i>
+                            Assunto: <i>{conteudo.assunto}</i>
                         </span>
                         <span>
-                            Cadastrado em: <i>27/05/2024</i>
+                            Cadastrado em: <i>{conteudo.createdFormat}</i>
                         </span>
                     </div>
                     <div className='row'>
                         <span>
-                            Status: <i>Aberto</i>
+                            Status: 
+                            <i className='status-bdge' 
+                            style={{
+                                color: '#FFF', 
+                                backgroundColor: conteudo.status === 'Aberto' ? '#5cb85c' : '#999'
+                                }}>
+                                {conteudo.status}
+                            </i>
                         </span>
                     </div>
-                    <>
-                    <h3>complemento</h3>
-                    <p>
-                        Aqui vai todo o complemento do chamado
-                    </p>
-                    </>
+                    {conteudo.complemento !== '' && (
+                        <>
+                        <h3>complemento</h3>
+                        <p>
+                            {conteudo.complemento}
+                        </p>
+                        </>
+                    )}
                 </main>
             </div>
         </div>
